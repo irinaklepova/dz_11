cd3e94246ae56e250a13059abd02c5ce38cf210a
Irina Klepova <ivklepova23@gmail.com>
2023-09-03 18:54:57 +0400

    add function description

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def make_capital_letters(value):
+    """Делает все буквы заглавными"""
     return value.upper()
+
+
+def title_letters(value):
+    """Делает первые буквы заглавными"""
+    return value.title()
 
